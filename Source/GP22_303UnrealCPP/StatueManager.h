73d30f9497520131a746ca82295986967773964e
Stewart Wan <112468923+nice1stu@users.noreply.github.com>
2023-07-27 23:20:15 +0200

    Create StatueManager Class

---
+++ Source/GP22_303UnrealCPP/StatueManager.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Info.h"
+#include "StatueManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GP22_303UNREALCPP_API AStatueManager : public AInfo
+{
+	GENERATED_BODY()
+	
+};
